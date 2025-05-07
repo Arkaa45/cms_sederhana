552669e9512489f9e86a79735d253bdc64b73f2f
Arkaa45 <rakakagem@gmail.com>
2025-05-07 17:38:07 +0700

    Commit 7
    Mengganti Icon dan Penyesuaian lainnya

--- index.php
+++ index.php
@@ -144,7 +144,7 @@ $posts = $stmt->fetchAll();
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
             <a href="index.php" class="brand-link">
-                <img src="https://adminlte.io/themes/v3/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+                <i class="fas fa-clipboard-list brand-image img-circle elevation-3" style="opacity: .8; font-size: 1.5rem; padding: 0.5rem;"></i>
                 <span class="brand-text font-weight-light">Simple CMS</span>
             </a>
 
